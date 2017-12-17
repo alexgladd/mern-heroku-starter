69f6011d6705705dee947a567fb61f90be53b4a6
Alex Gladd <alex@alexgladd.com>
2017-12-17 12:08:55 -0500

    Change auth tokens to expire after one year
    I.e., punt on handling re-auth workflow :)

--- api/controllers/auth/jwt.js
+++ api/controllers/auth/jwt.js
@@ -3,7 +3,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateUserToken = (user) => {
-  return jwt.sign({ user }, process.env.SECRET_KEY, { expiresIn: '1d' });
+  return jwt.sign({ user }, process.env.SECRET_KEY, { expiresIn: '1y' });
 }
 
 module.exports = generateUserToken;
